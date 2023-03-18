6571002c78b2adfee3845c18e8fc5a6cad86e161
NiShiGara <91442220+NiShiGara@users.noreply.github.com>
2023-03-18 21:59:02 +1000

    Проверки форм

---
+++ forms.py
@@ -0,0 +1,16 @@
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+
+
+class AuthorizationForm(Form):
+    auth_login = StringField("Логин", [validators.InputRequired()])
+    auth_password = PasswordField("Пароль", [validators.InputRequired()])
+
+class RegistrationForm(Form):
+    reg_login = StringField("Логин", [validators.InputRequired(), validators.Length(min=4, max=25)])
+    reg_password = PasswordField('Пароль', [
+        validators.InputRequired(),
+        validators.Length(min=8, max=60),
+        validators.EqualTo('password_confirm', message='Пароли должны совпадать')
+    ])
+    password_confirm = PasswordField('Повторите пароль')
+
