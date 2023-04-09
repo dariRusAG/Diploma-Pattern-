4c7f4ef8b9a40417db5664d72dd5fa07b3259a63
dariRusAG <agapova.dr@students.dvfu.ru>
2023-04-09 16:19:49 +1000

    Вывод мерок

--- models/scheme_model.py
+++ models/scheme_model.py
@@ -7,8 +7,19 @@ def get_scheme_pattern(conn, index):
         pattern_id AS ID,
         pattern_name AS Название, 
         category_name AS Категория,
-        picture AS Изображение        
+        picture AS Изображение  
     FROM pattern
     JOIN category USING (category_id)
+    WHERE pattern_id == {index}
+    ''', conn)
+
+
+def get_measure_pattern(conn, index):
+    return pd.read_sql(f'''
+    SELECT 
+    measure_name AS Обозначение, 
+    measure_full_name AS Полное_название
+    FROM measure 
+    JOIN pattern_measure USING (measure_id)
     WHERE pattern_id == {index}
     ''', conn)
