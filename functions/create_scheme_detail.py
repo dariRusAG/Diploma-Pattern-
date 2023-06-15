a7d4abec7ce7cc0962f891b197d591fb5ebcef2d
dariRusAG <agapova.dr@students.dvfu.ru>
2023-06-15 16:32:25 +1000

    Небольшие исправления в построении

--- functions/create_scheme_detail.py
+++ functions/create_scheme_detail.py
@@ -1,82 +1,5 @@
-import math
-import matplotlib.pyplot as plt
-import numpy as np
 from models.model_general import get_detail_name
-from models.scheme_model import *
-from functions.bezie import Bezier
 from functions.create_scheme import *
-from matplotlib.backends.backend_pdf import PdfPages
-
-
-# Расчет координат кривых линий
-def calculate_line_curve(row, x_coord_line, y_coord_line, df_formula):
-    # список всех x и y отклонений кривых линий
-    x_deviation = list()
-    y_deviation = list()
-
-    try:
-        # Если отклонений несколько, то преобразование в список
-        if type(eval(f"{row['x_deviation']}")) == float or type(eval(f"{row['x_deviation']}")) == int:
-            x1_deviation = eval(f"{row['x_deviation']}", {}, df_formula)
-        else:
-            x1_deviation = f"{row['x_deviation']}".split(",")
-            for num in range(len(x1_deviation)):
-                x1_deviation[num] = float(x1_deviation[num])
-        if type(eval(f"{row['y_deviation']}")) == float or type(eval(f"{row['y_deviation']}")) == int:
-            y1_deviation = eval(f"{row['y_deviation']}", {}, df_formula)
-        else:
-            y1_deviation = f"{row['y_deviation']}".split(",")
-            for num in range(len(y1_deviation)):
-                y1_deviation[num] = float(y1_deviation[num])
-    except NameError:
-        return "error_line"
-
-    x_deviation.append(x1_deviation)
-    y_deviation.append(y1_deviation)
-
-    # Построение линий
-    for i in range(0, len(x_coord_line) - 1, 2):
-        t_points = np.arange(0, 1, 0.009)
-
-        j = int(i / 2)
-        list_new_x = []
-        list_new_y = []
-
-        # если отклонение одно
-        if type(x_deviation[j]) == float or type(x_deviation[j]) == int:
-            d_x_first = x_deviation[j]
-            d_y_first = y_deviation[j]
-
-            list_new_x.append(((x_coord_line[i] + x_coord_line[i + 1]) / 2) * d_x_first)
-            list_new_y.append(((y_coord_line[i] + y_coord_line[i + 1]) / 2) * d_y_first)
-
-        # если отклонений несколько
-        else:
-            k = len(x_deviation[j])
-            for r in range(1, k + 1):
-                d_x_first = x_deviation[j][r - 1]
-                d_y_first = y_deviation[j][r - 1]
-
-                # формула расчета координат в отношении
-                alpha = r / (k + 1 - r)
-                list_new_x.append(((x_coord_line[i] + alpha * x_coord_line[i + 1]) / (1 + alpha)) * d_x_first)
-                list_new_y.append(((y_coord_line[i] + alpha * y_coord_line[i + 1]) / (1 + alpha)) * d_y_first)
-
-        # список всех отклонений прямой
-        all_deviations = []
-        for j in range(len(list_new_x)):
-            all_deviations.append([list_new_x[j], list_new_y[j]])
-
-        # список всех точек для построения
-        points_list = [[x_coord_line[i], y_coord_line[i]]]
-        points_list = points_list + all_deviations
-        points_list.append(
-            [x_coord_line[i + 1], y_coord_line[i + 1]]
-        )
-        points1 = np.asarray(points_list)
-
-    curve1 = Bezier.Curve(t_points, points1)
-    return curve1, points1
 
 
 def create_user_scheme_detail(conn, user_param, id_detail, pdf, func_name):
@@ -136,7 +59,7 @@ def create_user_scheme_detail(conn, user_param, id_detail, pdf, func_name):
             y_coord_line_straight.append(y_coord_line)
         else:
             try:
-                curve, points_curve = calculate_line_curve(row, x_coord_line, y_coord_line, df_formula)
+                curve, points_curve = calculate_line_curve(row, x_coord_line, y_coord_line, df_formula, 0, 0)
             except ValueError:
                 return "error_line"
             line_curve.append(curve)
--- functions/create_scheme_pattern.py
+++ functions/create_scheme_pattern.py
@@ -1,82 +1,4 @@
-import math
-import matplotlib.pyplot as plt
-import numpy as np
-
-from models.scheme_model import *
-from functions.bezie import Bezier
-from matplotlib.backends.backend_pdf import PdfPages
 from functions.create_scheme import *
-
-# Расчет координат кривых линий
-def calculate_line_curve_pattern(row, x_coord_line, y_coord_line, df_formula, x_shift, y_shift):
-    # список всех x и y отклонений кривых линий
-    x_deviation = list()
-    y_deviation = list()
-
-    try:
-        # Если отклонений несколько, то преобразование в список
-        if type(eval(f"{row['x_deviation']}")) == float or type(eval(f"{row['x_deviation']}")) == int:
-            x1_deviation = eval(f"{row['x_deviation']}", {}, df_formula)
-        else:
-            x1_deviation = f"{row['x_deviation']}".split(",")
-            for num in range(len(x1_deviation)):
-                x1_deviation[num] = float(x1_deviation[num])
-        if type(eval(f"{row['y_deviation']}")) == float or type(eval(f"{row['y_deviation']}")) == int:
-            y1_deviation = eval(f"{row['y_deviation']}", {}, df_formula)
-        else:
-            y1_deviation = f"{row['y_deviation']}".split(",")
-            for num in range(len(y1_deviation)):
-                y1_deviation[num] = float(y1_deviation[num])
-    except NameError:
-        return "error_line"
-
-    x_deviation.append(x1_deviation)
-    y_deviation.append(y1_deviation)
-
-    # Построение линий
-    for i in range(0, len(x_coord_line) - 1, 2):
-        t_points = np.arange(0, 1, 0.009)
-
-        j = int(i / 2)
-        list_new_x = []
-        list_new_y = []
-
-        # если отклонение одно
-        if type(x_deviation[j]) == float or type(x_deviation[j]) == int:
-            d_x_first = x_deviation[j]
-            d_y_first = y_deviation[j]
-
-            list_new_x.append(((x_coord_line[i] - x_shift + x_coord_line[i + 1] - x_shift) / 2) * d_x_first + x_shift)
-            list_new_y.append(((y_coord_line[i] - y_shift + y_coord_line[i + 1] - y_shift) / 2) * d_y_first + y_shift)
-
-        # если отклонений несколькоy_shift
-        else:
-            k = len(x_deviation[j])
-            for r in range(1, k + 1):
-                d_x_first = x_deviation[j][r - 1]
-                d_y_first = y_deviation[j][r - 1]
-
-                # формула расчета координат в отношении
-                alpha = r / (k + 1 - r)
-                list_new_x.append(((x_coord_line[i] - x_shift + alpha * (x_coord_line[i + 1] - x_shift)) / (1 + alpha)) * d_x_first + x_shift)
-                list_new_y.append(((y_coord_line[i] - y_shift + alpha * (y_coord_line[i + 1] - y_shift)) / (1 + alpha)) * d_y_first + y_shift)
-
-        # список всех отклонений прямой
-        all_deviations = []
-        for j in range(len(list_new_x)):
-            all_deviations.append([list_new_x[j], list_new_y[j]])
-
-        # список всех точек для построения
-        points_list = [[x_coord_line[i], y_coord_line[i]]]
-        points_list = points_list + all_deviations
-        points_list.append(
-            [x_coord_line[i + 1], y_coord_line[i + 1]]
-        )
-        points1 = np.asarray(points_list)
-
-    curve1 = Bezier.Curve(t_points, points1)
-    return curve1, points1
-
 
 def create_user_scheme_pattern(conn, user_param, list_id_detail, name_pattern, func_name):
     # создание словаря формул
@@ -182,7 +104,7 @@ def create_user_scheme_pattern(conn, user_param, list_id_detail, name_pattern, f
             y_coord_line_straight.append(y_coord_line)
         else:
             try:
-                curve, points_curve = calculate_line_curve_pattern(row, x_coord_line, y_coord_line, df_formula, x_shift, y_shift)
+                curve, points_curve = calculate_line_curve(row, x_coord_line, y_coord_line, df_formula, x_shift, y_shift)
             except ValueError:
                 return "error_line"
 
@@ -210,8 +132,8 @@ def create_user_scheme_pattern(conn, user_param, list_id_detail, name_pattern, f
     else:
         setting_plt(length_y + 2)
 
-    # if func_name != "admin":
-    #     plt.axis('off')
+    if func_name != "admin":
+        plt.axis('off')
 
     # количество листов по иксу
     pages_x = math.ceil(length_x / 21)
